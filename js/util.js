b0af24f252b02fb7c9d842691ec6c9f7ce4aeb4f
qurihara <qurihara@unryu.org>
2019-06-25 18:23:07 -0700

    ok

--- js/util.js
+++ js/util.js
@@ -1,16 +1,7 @@
-function s(dsl){
-  send(dsl);
-}
-function wait(delay) {
-    return new Promise(function(resolve, reject) {
-        setTimeout(resolve, delay);
-    });
-}
-
 $x = async function()
 {
     for(let i = 0; i < arguments.length; i++) {
-      	if (Array.sArray(arguments[i]) === true){
+      	if (Array.isArray(arguments[i]) === true){
             await $x(arguments[i])
         }else{
           await send(arguments[i])
@@ -18,9 +9,26 @@ $x = async function()
     }
 }
 
+function wait(delay) {
+    return new Promise(function(resolve, reject) {
+        setTimeout(resolve, delay);
+    });
+}
+
 t = function(_t){
       return {"dur":_t};
 }
+
 dp = function(_t){
       return {"dpad":_t};
+}
+
+bn = function(_b){
+  if (Array.isArray(_b) === true){
+    return {"btn":_b};
+  }else{
+    let a = [];
+    a[0] = _b;
+    return {"btn":a}
+  }
 }
